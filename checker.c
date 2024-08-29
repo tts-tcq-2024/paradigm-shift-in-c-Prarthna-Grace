10c9a5ca9d5f64fe00d13db9b9486d21f995c85e
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-08-30 00:26:11 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,12 +4,8 @@
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   if (temperature < TEMP_MIN || temperature > TEMP_MAX) {
     printf("Temperature out of range!\n");
-    return 0;
-  }
   if (soc < SOC_MIN || soc > SOC_MAX) {
     printf("State of Charge out of range!\n");
-    return 0;
-  }
   if (chargeRate > CHARGE_RATE_MAX) {
     printf("Charge Rate out of range!\n");
     return 0;
@@ -20,5 +16,4 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
-  assert(!batteryIsOk(35, 95, 0));
 }
