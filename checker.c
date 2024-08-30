e567fb43c0edb07b10d0afaac9c4e55102f55fb6
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-08-30 23:20:33 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -10,20 +10,21 @@ int checkCondition(float value, float min, float max, const char* message) {
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    int a= checkCondition(temperature, 0, 45, "Temperature out of range!");
-    
-    int b = checkCondition(soc, 20, 80, "State of Charge out of range!");
-    
-    if (chargeRate > 0.8) {
-        printf("Charge Rate out of range!\n");
-    }
+    int a = checkCondition(temperature, 0, 45, "Temperature out of range!\n");
+    int b = checkCondition(soc, 20, 80, "State of Charge out of range!\n");
+    int chargeRateOk = (chargeRate <= 0.8);
 
-    // Return 0 if either a or b is 0, otherwise return 1
-    if (a == 0 || b == 0 || chargeRate > 0.8) {
+    // Consolidate all conditions and return 0 if any check fails
+    if (!a || !b || !chargeRateOk) {
+        if (!chargeRateOk) {
+            printf("Charge Rate out of range!\n");
+        }
         return 0;
     }
+
     return 1;
 }
+
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
