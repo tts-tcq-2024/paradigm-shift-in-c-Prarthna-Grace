dc9a748683e5714ad386d92fb04e88f2d745b084
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-08-30 23:15:25 +0530

    Update checker.c
    reducing the if conditions

--- checker.c
+++ checker.c
@@ -3,22 +3,20 @@
 
 int checkCondition(float value, float min, float max, const char* message) {
     if (value < min || value > max) {
-        printf("%s\n", message);
+        printf("%s", message);
         return 0;
     }
     return 1;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    if (!checkCondition(temperature, 0, 45, "Temperature out of range!")) {
-        return 0;
-    }
-    if (!checkCondition(soc, 20, 80, "State of Charge out of range!")) {
-        return 0;
-    }
-    if (chargeRate > 0.8) {
+    int a= checkCondition(temperature, 0, 45, "Temperature out of range!");
+    
+    int b = checkCondition(soc, 20, 80, "State of Charge out of range!");
+    
+    if ((chargeRate > 0.8) {
         printf("Charge Rate out of range!\n");
-        return 0;
+        if( a==0 && b==0) return 0;
     }
     return 1;
 }
