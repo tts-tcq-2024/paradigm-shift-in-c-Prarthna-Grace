55786975bb470f781f5a8a5164ef83549c8f2c4e
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-08-30 22:51:38 +0530

    Update checker.c
    Addition of a function

--- checker.c
+++ checker.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <assert.h>
 
+int checkCondition(float value, float min, float max, const char* message) {
+    if (value < min || value > max) {
+        printf("%s\n", message);
+        return 0;
+    }
+    return 1;
+}
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+    if (!checkCondition(temperature, 0, 45, "Temperature out of range!")) {
+        return 0;
+    }
+    if (!checkCondition(soc, 20, 80, "State of Charge out of range!")) {
+        return 0;
+    }
+    if (chargeRate > 0.8) {
+        printf("Charge Rate out of range!\n");
+        return 0;
+    }
+    return 1;
 }
 
 int main() {
