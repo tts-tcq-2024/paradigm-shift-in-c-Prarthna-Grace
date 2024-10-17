4690fbf783d2de6268f15a03148617feb787d3b8
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-17 13:31:28 +0530

    Create check_warning.c

---
+++ check_warning.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<checker.c>
+
+int checkWarning(float value, float min, float max) {
+    float warningThreshold = 0.05 * max; // 5% of the max value
+    if (value >= min && value < (min + warningThreshold)) {
+        display("Warning: Approaching discharge\n");
+        return 1; // Indicates a warning
+    } else if (value <= max && value > (max - warningThreshold)) {
+        display("Warning: Approaching charge-peak\n");
+        return 1; // Indicates a warning
+    }
+    return 0; // No warning
+}
