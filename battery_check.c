f448dd0b92176931ab1a0e7e05ca7ecb54614322
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-17 13:25:30 +0530

    Create battery_check.c

---
+++ battery_check.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    int isokay = 1;
+
+    isokay &= checkCondition(temperature, 0, 45, "Temperature out of range!\n");
+    isokay &= checkCondition(soc, 20, 80, "State of Charge out of range!\n");
+    
+    if (chargeRate > 0.8) {
+        display("Charge Rate out of range!\n");
+        isokay = 0;
+    }
+
+    return isokay;
+}
