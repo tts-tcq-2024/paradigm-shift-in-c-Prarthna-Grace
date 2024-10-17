50ac3b2140974b2023c5b82703b795f54ea8c967
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-17 15:06:53 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,10 +7,22 @@ void display(const char* message){
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));  
-  assert(!batteryIsOk(50, 85, 0));  
-  assert(!batteryIsOk(25, 85, 0));   
-  assert(!batteryIsOk(50, 70, 0));  
-  assert(!batteryIsOk(25, 70, 0.9)); 
-  assert(!batteryIsOk(50, 80, 0.7)); 
+    // Normal case
+    assert(batteryIsOk(25, 70, 0.7)); 
+    // Temperature out of range
+    assert(!batteryIsOk(50, 85, 0)); 
+    // State of Charge out of range
+    assert(!batteryIsOk(25, 85, 0)); 
+     // Temperature out of range
+    assert(!batteryIsOk(50, 70, 0));  
+     // Charge rate out of range
+    assert(!batteryIsOk(25, 70, 0.9)); 
+    // Temperature out of range with valid SoC
+    assert(!batteryIsOk(50, 80, 0.7));
+    // Approaching discharge warning for SoC
+    assert(batteryIsOk(25, 19.9, 0.7)); // Should be okay but with a warning
+    // Approaching charge-peak warning for SoC
+    assert(batteryIsOk(25, 79.9, 0.7)); // Should be okay but with a warning
+    // Charge rate approaching maximum (0.75)
+    assert(batteryIsOk(25, 70, 0.75)); // Should be okay but with a warning
 }
