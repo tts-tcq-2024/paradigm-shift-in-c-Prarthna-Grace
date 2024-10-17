8debc433ddab603cacdad3c29985d704a44632a2
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-17 13:26:22 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 #include<battery_checck.c>
+#include<check_condition.c>
 
 void display(const char* message){
     printf("%s", message);
-}
-
-int checkCondition(float value, float min, float max, const char* message) {
-    if (value < min || value > max) {
-        display(message);
-        return 0;
-    }
-    return 1;
 }
 
 int main() {
