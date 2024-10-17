989f4ffdcbc80159ae0e703eb83ddbbc0e287790
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-17 13:49:04 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -4,13 +4,23 @@
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     int isokay = 1;
 
+     // Check temperature and display warnings if needed
     isokay &= checkCondition(temperature, 0, 45, "Temperature out of range!\n");
+    checkWarning(temperature, 0, 45);
+    
+     // Check SoC and display warnings if needed
     isokay &= checkCondition(soc, 20, 80, "State of Charge out of range!\n");
+    checkWarning(soc, 20, 80);
+
+    // Check charge rate and warning
+    if (checkChargeRateWarning(chargeRate)) {
+        isokay = 0; // If there's a warning, mark it as not okay
+    }
     
     if (chargeRate > 0.8) {
         display("Charge Rate out of range!\n");
         isokay = 0;
     }
 
-    return isokay;
+    return isokay; // Return 1 if all conditions are met, otherwise 0
 }
