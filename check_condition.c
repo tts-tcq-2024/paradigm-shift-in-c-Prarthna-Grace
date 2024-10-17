6da38a71e9347208b078e40a5479b30352b362db
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-17 14:04:28 +0530

    Update check_condition.c

--- check_condition.c
+++ check_condition.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
 #include"checker.h"
 
-void display(const char* message) {
-    printf("%s", message);
-}
-
 int checkCondition(float value, float min, float max, const char* message) {
     if (value < min || value > max) {
         display(message);
