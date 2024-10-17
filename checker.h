30703fc2b7c153bd8e9b6a4fb58edfad2bd927ac
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-17 15:37:05 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -5,7 +5,8 @@
 int batteryIsOk(float temperature, float soc, float chargeRate);
 void display(const char* message);
 int checkCondition(float value, float min, float max, const char* message);
-int checkWarning(float value, float min, float max);
+int checkSocWarning(float value, float min, float max);
 int checkChargeRateWarning(float chargeRate);
+int checkTemperatureWarning(float temperature, float min, float max);
 
 #endif // CHECKER_H
